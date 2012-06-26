f54b62c35ca3994958e821f7c96b492f60c5cd38
Mick Brooks <mick.brooks@sinking.in>
2012-06-26 14:28:26 +0100

    My intuition wants me to push-down the contains and union behaviour to the subtypes. Do we still have an ADT?

--- src/in/sinking/ADTExample/CSet.java
+++ src/in/sinking/ADTExample/CSet.java
@@ -15,46 +15,43 @@ public abstract class CSet {
 		}
 	}
 		
-	public boolean contains(int n) {
-		if (this instanceof Empty) {
+	public abstract boolean contains(int n);
+	public abstract CSet union(CSet b);
+
+	static private class Empty extends CSet {
+		@Override
+		public boolean contains(int n) {
 			return false;
 		}
-		else if (this instanceof Pair) {
-			final Pair rep = (Pair) this;
-			if (rep.first == n) {
-				return true;
-			}
-			else {
-				return rep.rest.contains(n);
-			}
-		}
-		else {
-			throw new IllegalStateException("Someone changed my rep without letting me know!");
-		}
-	}
-	
-	public CSet union(CSet b) {
-		if (this instanceof Empty) {
+		
+		@Override
+		public CSet union(CSet b) {
 			return b;
 		}
-		else if (this instanceof Pair) {
-			final Pair rep = (Pair) this;
-			return rep.rest.union(b).insert(rep.first);
-		}
-		else {
-			throw new IllegalStateException("Someone changed my rep without letting me know!");
-		}
 	}
-
-	static private class Empty extends CSet { }
 	
 	static private class Pair extends CSet {
-		public int first;
-		public CSet rest;
+		private int first;
+		private CSet rest;
 		
 		public Pair(int first, CSet rest) {
 			this.first = first;
 			this.rest = rest;
+		}
+		
+		@Override
+		public boolean contains(int n) {
+			if (first == n) {
+				return true;
+			}
+			else {
+				return rest.contains(n);
+			}
+		}
+		
+		@Override
+		public CSet union(CSet b) {
+			return rest.union(b).insert(first);
 		}
 	}
 }
